7038e82b3b34158184db81b87174cd65d34e1842
Alfelix5 <108706938+Alfelix5@users.noreply.github.com>
2022-07-31 10:30:47 -0300

    Add files via upload

---
+++ Teste.java
@@ -0,0 +1,13 @@
+package escola;
+
+public class Teste {
+    public static void main(String[] args) {
+        
+        Aluno aluno = new Aluno();
+        aluno.setNome("arex");
+
+    }  
+
+    
+}
+
